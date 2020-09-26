66a93579e19728d6c882bb1b7bac9e8d3da13bb9
Robsongap <robsonmie@hotmail.com>
2020-09-26 16:06:45 -0300

    Gerando infromações de upload e carregamento de arquivos close#38
    Será gerado informações do arquivo nome, tamanho etc.. sera gerado uma lista com informações dos arquivos, a barra de upload mostra a percentagem do upload e lista os arquivos.

--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -5,40 +5,46 @@ import { MdCheckCircle, MdError, MdLink }  from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './style';
 
-const FileList = () => (
+const FileList = ( { files } ) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src=""/>
+    {files.map(uploadedFile => (
+      <li>
+        <FileInfo>
+          <Preview src={uploadedFile.preview}/>
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>{uploadedFile.readableSize}<button onClick={() => {}}>Excluir</button></span>
+          </div>
+        </FileInfo>
+
         <div>
-          <strong>profile.png</strong>
-          <span>64kb<button onClick={() => {}}>Excluir</button></span>
+          {!uploadedFile.uploaded && !uploadedFile.erro && (
+            <CircularProgressbar
+            styles={{
+              root: { width: 24 },
+              path: { stroke: '#7159c1' }
+            }}
+            strokeWidth={10}
+            percentage={uploadedFile.progress}
+            />
+          )}
+
+            {uploadedFile.url && (
+              <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+  
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+              </a>
+            )}
+
+            { uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+            { uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
-      </FileInfo>
-
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: '#7159c1' }
-          }}
-          strokeWidth={10}
-          percentage={30}
-          />
-
-          <a
-           href=""
-           target="_blank"
-           rel="noopener noreferrer"
-           >
-
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-           </a>
-
-           <MdCheckCircle size={24} color="#78e5d5" />
-           <MdError size={24} color="#e57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
